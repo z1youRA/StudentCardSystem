5db8b6868d877e6b8f8944ab34c670fc98d7afe3
z1youRA <374671110@qq.com>
2022-02-12 19:09:18 +0800

    finish initCard function

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 #define DELETED 0
 #define NORMAL 1
 #define LOST 2
@@ -17,7 +17,6 @@ typedef struct card {
 typedef struct student {
     char name[20];
     long studentNum;
-    int balance;
     Card* front;
     Card* rear;
     int status;
@@ -33,7 +32,7 @@ float balanceToFloat(int balance) {
 }
 
 //初始化学生结构，返回指向该学生的指针
-Student* initStu(char* name, int studentNum, int balance, int pwd) {
+Student* initStu(char* name, long studentNum, int pwd) {
     Student* stu;
     int i = 0;
     stu = (Student*)malloc(sizeof(Student));
@@ -46,13 +45,29 @@ Student* initStu(char* name, int studentNum, int balance, int pwd) {
     }
     stu->name[i] = '\0';
     stu->studentNum = studentNum;
-    stu->balance = balance;
     stu->front = NULL;
     stu->rear = NULL;
     stu->status = NORMAL;
     return stu;
 }
 
+Card* initCard(long studentNum, int status, float balance,int expDate, int pwd) {
+    Card* card;
+    card = (Card*)malloc(sizeof(Card));
+    if(card == NULL) {
+        printf("动态空间申请失败!\n");
+        exit(1);
+    } 
+    int abc = (int)(balance * 100);
+    card->balance = abc;
+    card->expDate = expDate;
+    card->status = status;
+    card->studentNum = studentNum;
+    card->pwd = pwd;
+    return card;
+}
+
 int main() {
+    initCard(123, NORMAL, 100.12, EXPDATE, 8888);
     return 0;
 }
